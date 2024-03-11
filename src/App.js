8c6d8889b22254f4adae26a620e47afc7ca0415e
Jahongir_Nusratov <nusratovjahongir@gmail.com>
2024-03-11 10:14:25 +0500

    feat: build basic routes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Basket from './pages/basket/Basket';
+import Catalog from './pages/catalog/Catalog';
+import Contacts from './pages/contacts/Contacts';
+import News from './pages/news/News';
+import Products from './pages/products/Products';
+
 function App() {
 	return (
-		<div>
-			<h1>AntikDecor</h1>
-		</div>
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/about' element={<About />} />
+			<Route path='/basket' element={<Basket />} />
+			<Route path='/catalog' element={<Catalog />} />
+			<Route path='/contacts' element={<Contacts />} />
+			<Route path='/news' element={<News />} />
+			<Route path='/news' element={<Products />} />
+		</Routes>
 	);
 }
 
